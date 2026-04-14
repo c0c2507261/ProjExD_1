f54a96e506bb37588b2b21ce4b217d6f309d76ae
kouji <c0c2507261@edu.teu.ac.jp>
2026-04-14 15:31:14 +0900

    はばたけこうかとん基本実装

---
+++ pygame_image.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img_flip = pg.transform.flip(bg_img, True, False)
+
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)  # ← バグ1修正
+
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+
+    clock = pg.time.Clock()
+    tmr = 0
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        # ↓ ここから全部 while の中（バグ2修正：インデント）
+        x = tmr % 3200
+        screen.blit(bg_img,      [-x,        0])
+        screen.blit(bg_img_flip, [-x + 1600, 0])
+        screen.blit(bg_img,      [-x + 3200, 0])
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, 1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(1, 0)
+
+        screen.blit(kk_img, kk_rct)
+
+        pg.display.update()
+        tmr += 1
+        clock.tick(200)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
